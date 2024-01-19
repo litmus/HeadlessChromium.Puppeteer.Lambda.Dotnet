f0d854ac48ac9da1ebc81a409d230f6a2cd3f9bf
Brian Feucht <brian@litmus.com>
2024-01-19 13:33:25 -0800

    Turn on debug dumping to get chromium logs

--- sample/SampleLambda-dotnet8/HelloWorldHandler.cs
+++ sample/SampleLambda-dotnet8/HelloWorldHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Amazon.Lambda.Core;
 using HeadlessChromium.Puppeteer.Lambda.Dotnet;
 using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
 
 namespace SampleLambda
 {
@@ -14,10 +15,24 @@ namespace SampleLambda
 
             var launchArgs = HeadlessChromiumPuppeteerLauncher.DefaultChromeArgs
                 .Append("--enable-logging")
-                .Append("--v=1")
+                .Append("--log-level=0")
+                .Append("--single-process")
                 .ToArray();
 
-            await using (var browser = await browserLauncher.LaunchAsync(launchArgs))
+
+            var chromeLocation = new ChromiumExtractor(loggerFactory).ExtractChromium();
+
+            var launchOptions = new LaunchOptions()
+            {
+                ExecutablePath = chromeLocation,
+                Args = launchArgs,
+                Headless = true,
+                DumpIO = true,
+                EnqueueTransportMessages = false,
+                UserDataDir = "/tmp/",
+            };
+            
+            await using (var browser = await new Launcher(loggerFactory).LaunchAsync(launchOptions))
             await using (var page = await browser.NewPageAsync())
             {
                 await page.GoToAsync("https://www.google.com");
